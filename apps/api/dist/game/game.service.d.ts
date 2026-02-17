17e36c5362d104cfd39b43470c3c95edb1ba5d35
jblancoh <zlatanjon@hotmail.com>
2026-02-16 22:49:59 -0600

    chore: update gitignore and clean cache

--- apps/api/dist/game/game.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { GameState, Player, Vector2 } from '@barrilitospace/shared';
-export declare class GameService {
-    private gameState;
-    addPlayer(id: string, name: string, avatarUrl?: string): Player;
-    removePlayer(id: string): void;
-    movePlayer(id: string, position: Vector2): void;
-    getGameState(): GameState;
-}
--- apps/api/dist/livekit/livekit.controller.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class LivekitController {
-    getToken(room: string, username: string): Promise<{
-        token: string;
-        url: string;
-    }>;
-}
